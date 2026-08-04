54248735478ff46d23e175ac2ff0704504643e71
erichvonjagow <erichvonjagow@gmail.com>
2026-08-04 00:23:55 -0400

    Fix

--- out/html/game.js
+++ out/html/game.js
@@ -17,147 +17,6 @@
   };
 
   var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
-
-  // the url is a link to game.json
-  // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
-  // TODO; 
-  window.loadMod = function(url) {
-      ui.loadGame(url);
-  };
-
-  window.showStats = function() {
-    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('library')) {
-        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
-    } else {
-        window.dendryUI.dendryEngine.goToScene('library');
-    }
-  };
-
-  window.showMods = function() {
-    window.hideOptions();
-    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('mod_loader')) {
-        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
-    } else {
-        window.dendryUI.dendryEngine.goToScene('mod_loader');
-    }
-  };
-  
-  window.showOptions = function() {
-      var save_element = document.getElementById('options');
-      window.populateOptions();
-      save_element.style.display = "block";
-      if (!save_element.onclick) {
-          save_element.onclick = function(evt) {
-              var target = evt.target;
-              var save_element = document.getElementById('options');
-              if (target == save_element) {
-                  window.hideOptions();
-              }
-          };
-      }
-  };
-
-  window.hideOptions = function() {
-      var save_element = document.getElementById('options');
-      save_element.style.display = "none";
-  };
-
-  window.disableBg = function() {
-      window.dendryUI.disable_bg = true;
-      document.body.style.backgroundImage = 'none';
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableBg = function() {
-      window.dendryUI.disable_bg = false;
-      window.dendryUI.setBg(window.dendryUI.dendryEngine.state.bg);
-      window.dendryUI.saveSettings();
-  };
-
-  window.disableAnimate = function() {
-      window.dendryUI.animate = false;
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableAnimate = function() {
-      window.dendryUI.animate = true;
-      window.dendryUI.saveSettings();
-  };
-
-  window.disableAnimateBg = function() {
-      window.dendryUI.animate_bg = false;
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableAnimateBg = function() {
-      window.dendryUI.animate_bg = true;
-      window.dendryUI.saveSettings();
-  };
-
-  window.disableAudio = function() {
-      window.dendryUI.toggle_audio(false);
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableAudio = function() {
-      window.dendryUI.toggle_audio(true);
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableImages = function() {
-      window.dendryUI.show_portraits = true;
-      window.dendryUI.saveSettings();
-  };
-
-  window.disableImages = function() {
-      window.dendryUI.show_portraits = false;
-      window.dendryUI.saveSettings();
-  };
-
-  window.enableLightMode = function() {
-      window.dendryUI.dark_mode = false;
-      document.body.classList.remove('dark-mode');
-      window.dendryUI.saveSettings();
-  };
-  window.enableDarkMode = function() {
-      window.dendryUI.dark_mode = true;
-      document.body.classList.add('dark-mode');
-      window.dendryUI.saveSettings();
-  };
-
-  // populates the checkboxes in the options view
-  window.populateOptions = function() {
-    var disable_bg = window.dendryUI.disable_bg;
-    var animate = window.dendryUI.animate;
-    var disable_audio = window.dendryUI.disable_audio;
-    var show_portraits = window.dendryUI.show_portraits;
-    if (disable_bg) {
-        $('#backgrounds_no')[0].checked = true;
-    } else {
-        $('#backgrounds_yes')[0].checked = true;
-    }
-    if (animate) {
-        $('#animate_yes')[0].checked = true;
-    } else {
-        $('#animate_no')[0].checked = true;
-    }
-    if (disable_audio) {
-        $('#audio_no')[0].checked = true;
-    } else {
-        $('#audio_yes')[0].checked = true;
-    }
-    if (show_portraits) {
-        $('#images_yes')[0].checked = true;
-    } else {
-        $('#images_no')[0].checked = true;
-    }
-    if (window.dendryUI.dark_mode) {
-        $('#dark_mode')[0].checked = true;
-    } else {
-        $('#light_mode')[0].checked = true;
-    }
-  };
-
   
   // This function allows you to modify the text before it's displayed.
   // E.g. wrapping chat-like messages in spans.
@@ -187,21 +46,6 @@
       dendryUI.dendryEngine._runActions(scene.onArrival);
       var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
-  };
-
-  window.changeTab = function(newTab, tabId) {
-      if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
-          window.alert('Polls are not available in historical mode.');
-          return;
-      }
-      var tabButton = document.getElementById(tabId);
-      var tabButtons = document.getElementsByClassName('tab_button');
-      for (i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].className = tabButtons[i].className.replace(' active', '');
-      }
-      tabButton.className += ' active';
-      window.statusTab = newTab;
-      window.updateSidebar();
   };
 
   window.onDisplayContent = function() {
@@ -244,13 +88,5 @@
   window.statusTab = "status";
   window.dendryModifyUI = main;
   console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
-
-  window.onload = function() {
-    window.dendryUI.loadSettings({show_portraits: false});
-    if (window.dendryUI.dark_mode) {
-        document.body.classList.add('dark-mode');
-    }
-    window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
-  };
 
 }());
